b181c0e4ccfaaa1550773e3b8f6aa8e4458916e9
bybert <74455519+bybert@users.noreply.github.com>
2023-09-08 22:04:10 +0300

    new app

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+export function AboutPage() {
+    return (
+        <div>
+            This is Single Page Application project of online store written using React, TypeScript and Fake Store API
+        </div>
+    )
+}
